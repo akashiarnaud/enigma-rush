d5382d53e3aa6eef3c49d7912b63fd5c7f400588
arnaud <tokyarnaud@gmail.com>
2021-12-12 12:59:18 +0100

    features/scanble - add bluetooh page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Bluetooth from "../views/Bluetooth.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Bluetooth",
+    component: Bluetooth,
   },
   {
     path: "/about",
